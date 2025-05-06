aab96dd7cbcc9bed6ba0ed3abef1b37d37182efd
张诗余 <luckcool@vip.qq.com>
2025-05-06 10:50:06 +0800

    团结引擎版本

--- Unity/Assets/Scripts/Core/Network/KService.cs
+++ Unity/Assets/Scripts/Core/Network/KService.cs
@@ -253,7 +253,7 @@ namespace ET
                                 buffer.WriteTo(0, KcpProtocalType.RouterReconnectACK);
                                 buffer.WriteTo(1, kChannel.LocalConn);
                                 buffer.WriteTo(5, kChannel.RemoteConn);
-                                this.Transport.Send(buffer, 0, 9, this.ipEndPoint);
+                                this.Transport.Send(buffer, 0, 9, this.ipEndPoint, ChannelType.Accept);
                             }
                             catch (Exception e)
                             {
@@ -275,6 +275,10 @@ namespace ET
                             if (messageLength > 9)
                             {
                                 realAddress = this.cache.ToStr(9, messageLength - 9);
+                            }
+                            else
+                            {
+                                realAddress = this.ipEndPoint.ToString();    
                             }
 
                             remoteConn = BitConverter.ToUInt32(this.cache, 1);
@@ -298,7 +302,7 @@ namespace ET
                                 
                                 kChannel.RealAddress = realAddress;
 
-                                IPEndPoint realEndPoint = kChannel.RealAddress == null? kChannel.RemoteAddress : NetworkHelper.ToIPEndPoint(kChannel.RealAddress);
+                                IPEndPoint realEndPoint = NetworkHelper.ToIPEndPoint(kChannel.RealAddress);
                                 this.AcceptCallback(kChannel.Id, realEndPoint);
                             }
                             if (kChannel.RemoteConn != remoteConn)
@@ -321,7 +325,7 @@ namespace ET
                                 buffer.WriteTo(5, kChannel.RemoteConn);
                                 Log.Info($"kservice syn: {kChannel.Id} {remoteConn} {localConn} {kChannel.RemoteAddress}");
                                 
-                                this.Transport.Send(buffer, 0, 9, kChannel.RemoteAddress);
+                                this.Transport.Send(buffer, 0, 9, kChannel.RemoteAddress, ChannelType.Accept);
                             }
                             catch (Exception e)
                             {
@@ -482,7 +486,7 @@ namespace ET
                 buffer.WriteTo(9, (uint) error);
                 for (int i = 0; i < times; ++i)
                 {
-                    this.Transport.Send(buffer, 0, 13, address);
+                    this.Transport.Send(buffer, 0, 13, address, ChannelType.Accept);
                 }
             }
             catch (Exception e)
